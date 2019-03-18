8721d24d5dada34896b188ee752ef1d5b8c9c9e4
eg <eg@gmail.com>
2019-03-18 23:23:22 +0300

    Path added

--- src/main/java/com/training/eg/first/HelloWorldController.java
+++ src/main/java/com/training/eg/first/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.training.eg.first;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Controller
 @RestController
@@ -24,11 +21,14 @@ public class HelloWorldController {
     }
 
 
-
-
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
