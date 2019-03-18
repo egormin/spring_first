d7467110ea57a4ab921fb9c7bb04493490a7943e
eg <eg@gmail.com>
2019-03-18 22:29:03 +0300

    Hello world added

---
+++ src/main/java/com/training/eg/first/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.training.eg.first;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+    //GET
+    //URI - /hello-world
+
+    //Long way
+    //@RequestMapping(method= RequestMethod.GET, path="/hello-world")
+
+    //Short way
+    @GetMapping(path = "/hello-world")
+    //http://<ip>/hello-world
+    //Output: hello-world
+
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
